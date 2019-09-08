228e2e2dca34d58d993b5474d28396ec1f481485
Andrei <andreibu.ancora@gmail.com>
2019-09-08 17:42:11 +0300

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+import festivalOBjects.MainThread;
+
+public class Main {
+
+    public static void main(String[] args) {
+        MainThread mainThread = new MainThread();
+        mainThread.run();
+    }
+}
